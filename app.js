f950645d7fe3ce6234da5aa7ac9ffc9124366840
Ihor Romanenko <ihor@romanenko.dp.ua>
2016-05-12 22:37:11 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+var http = require('http'),
+    config = {
+      ip : '0.0.0.0',
+      port : 3343
+    };
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>It is alive!</h1>\n');
+}).listen(config.port, config.ip);
+
+console.log(`Server running on port ${config.port}.`);
